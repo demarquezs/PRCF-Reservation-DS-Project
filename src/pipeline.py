e5a4294e1351ff4c485c35ba5b296ee3858150e1
demarquezs <ingdemarquezs@outlook.com>
2025-11-15 14:24:37 -0500

    Correcting last test errors

--- src/pipeline.py
+++ src/pipeline.py
@@ -9,6 +9,7 @@ import mlflow
 @task
 def load_data_task():
 
+    #load the data
     df = load_data()
     df_filtered = filter_and_process_data(df)
     X_train_scaled, X_val, y_train, y_val = transform_and_split_data(df_filtered)
@@ -19,6 +20,7 @@ def load_data_task():
 @task
 def obtain_models_task():
 
+    #call the models
     models= get_models_to_use()
 
     return models
@@ -26,24 +28,23 @@ def obtain_models_task():
 
 @task
 def training_model_task(X_train_scaled, X_val, y_train, y_val, models):
+    #train the model
+    with mlflow.start_run(run_name="Training_All_Models"):
+        
+        #onecall function handles all models
+        best_name, best_model, weighted_scores = tscv_with_weighted_best_model(
+            X_train_scaled, X_val, y_train, y_val, models
+        )
 
-    with mlflow.start_run(run_name="Training_All_Models") as parent_run:
-        # Call once: tscv handles all models internally
-        best_name, best_model, weighted_scores = tscv_with_weighted_best_model(X_train_scaled, X_val, y_train, y_val, models)
-
-
-        # log the weighted scores for every model
+        #log weighted R² for every model
         for mname, score in weighted_scores.items():
             mlflow.log_metric(f"{mname}_weighted_r2", float(score))
 
-        # log model artifact
-        try:
-            mlflow.sklearn.log_model(best_model, artifact_path=f"models/{best_name}")
-        except Exception as e:
-            print(f"Model logging failed: {e}")
+        #og best model to MLflowl
+        mlflow.sklearn.log_model(best_model, artifact_path=f"models/{best_name}")
 
     return best_name, best_model, weighted_scores
-    
+
 
 @flow
 def mlops_pipeline_flow():
